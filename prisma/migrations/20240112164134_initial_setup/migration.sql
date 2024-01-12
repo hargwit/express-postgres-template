2eba978fed4a86ffca0f36e76bd3811a90aaa9d7
Harry G Withers <harrygwithers@gmail.com>
2024-01-12 16:42:06 +0000

    chore: setup prisma

---
+++ prisma/migrations/20240112164134_initial_setup/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Entity" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Entity_pkey" PRIMARY KEY ("id")
+);
--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 cp .env.example .env
+
+docker-compose up -d
+
+yarn db:migrate:local
